1cfa94cec6079c84a98a29140e00d3a6b90306b3
karolinawalach <karolina.walach@stud.uni-heidelberg.de>
2023-05-24 20:39:56 +0200

    z-transform and pca

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
-train_data = pd.read_csv()
-test_data = pd.read_csv()
+train_data = pd.read_csv("../mnist_train.csv")
+test_data = pd.read_csv("../mnist_test.csv")
 
 train_labels = train_data.iloc[:,0]
 train_data = train_data.drop(['0'], axis=1)
+
+test_labels = test_data.iloc[:,0]
+test_data = test_data.drop(['0'], axis=1)
 
 for i in range(10):
     ax= plt.subplot(1,10 ,i+1)
@@ -15,3 +20,16 @@ for i in range(10):
     ax.axes.get_yaxis().set_visible(False)
     plt.title(train_labels.iloc[i])
 plt.show()
+
+# Z-Transform for train and test datasets
+train_data_z = StandardScaler().fit_transform(train_data)
+test_data_z = StandardScaler().fit_transform(test_data)
+
+# PCA for train and test datasets
+pca = PCA(n_components=.95)
+pca.fit(train_data_z)
+pca.fit(test_data_z)
+train_data_pca = pca.transform(train_data_z)
+test_data_pca = pca.transform(test_data_z)
+
+print(train_data_pca)
